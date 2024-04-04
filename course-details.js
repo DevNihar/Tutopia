d3eb25231eb4be60d21cc0cc06a869de840332ab
DevNihar <110764445+DevNihar@users.noreply.github.com>
2024-04-05 01:30:31 +0530

    added header and footer to the course details page as well as fixed the drop down all categories section bug

---
+++ course-details.js
@@ -0,0 +1,72 @@
+"use strict";
+
+// Add fixed header when scrolling
+window.addEventListener("scroll", function () {
+  let navbar = document.querySelector("body header");
+  let headerHeight = document.querySelector("body header").offsetHeight;
+  const filler = document.getElementById("filler");
+
+  if (window.scrollY > headerHeight) {
+    filler.style.display = "block";
+    navbar.classList.add("fixed-header");
+    navbar.classList.add("animate__animated", "animate__slideInDown");
+    navbar.style.setProperty("animation-duration", "1s");
+  } else {
+    navbar.classList.remove(
+      "fixed-header",
+      "animate__animated",
+      "animate__slideInDown"
+    );
+    filler.style.display = "none";
+  }
+});
+
+// Hovering effects for socials icons
+document.querySelectorAll(".socials-icon-bg").forEach(function (btn) {
+  let img = btn.querySelector("img");
+  const imgName = img.id;
+  btn.addEventListener("mouseover", function () {
+    // Swap image source on hover
+    img.src = `images/${imgName}-white.png`;
+    btn.style.backgroundColor = "#ff723a";
+  });
+
+  btn.addEventListener("mouseout", function () {
+    // Revert image source on mouseout
+    img.src = `images/${imgName}.png`;
+    btn.style.backgroundColor = "white";
+  });
+});
+
+// Function to check if an element is in the viewport
+function isInViewport(element) {
+  let rect = element.getBoundingClientRect();
+  let viewportHeight =
+    window.innerHeight || document.documentElement.clientHeight;
+  return rect.top <= viewportHeight;
+}
+
+function footerCardFadeUp() {
+  let cards = document.querySelectorAll(".footer-cards");
+  let view = document.querySelector(".foot-mid .about-us");
+  let delay = 0;
+  if (isInViewport(view)) {
+    cards.forEach(function (card) {
+      card.classList.add("animate__animated", "animate__fadeInUp");
+      card.style.setProperty("animation-duration", "1s");
+      card.style.setProperty("animation-delay", `${delay}s`);
+      delay += 0.1;
+    });
+  }
+}
+
+// Function to handle scroll event
+function handleScroll() {
+  footerCardFadeUp();
+}
+
+// Attach scroll event listener
+window.addEventListener("scroll", handleScroll);
+
+// Initial check on page load
+handleScroll();
