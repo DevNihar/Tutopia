9fcef3f87f6a5fc42e8dd1f426091be64ac82347
DevNihar <110764445+DevNihar@users.noreply.github.com>
2024-04-07 22:27:52 +0530

    Started working on the courses page

--- courses.js
+++ courses.js
@@ -1,91 +1,4 @@
-("use strict");
-
-window.onload = function () {
-  fetchCourseData();
-  var swiper = new Swiper(".carousel", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    loop: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-      // Settings for screens smaller than 768px
-      0: {
-        slidesPerView: 1,
-        spaceBetween: 20,
-      },
-      // Settings for screens 768px and larger
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 30,
-      },
-      // Settings for screens 1200px and larger
-      1200: {
-        slidesPerView: 3,
-        spaceBetween: 30,
-      },
-    },
-  });
-};
-
-const fetchCourseData = () => {
-  fetch("http://localhost:3000/api/courses")
-    .then((response) => response.json())
-    .then((courses) => {
-      // Use the courses array to render the course cards on your homepage
-      renderCoursesOnpage(courses);
-    })
-    .catch((error) => {
-      console.error("Error fetching course data:", error);
-    });
-};
-
-const renderCoursesOnpage = (courses) => {
-  console.log(courses);
-  const coursesCards = document.querySelectorAll(".card");
-
-  coursesCards.forEach((card, index) => {
-    const feedbackTag = card.querySelector(".feedback-tag");
-    feedbackTag.innerHTML = `${courses[index].rating}<img src="images/star.png" />(${courses[index].no_of_people_rated})`;
-
-    // Update course type
-    const typeTag = card.querySelector(".tag-section span");
-    typeTag.textContent = courses[index].type;
-
-    // Update course name
-    const subtitle = card.querySelector(".title-section a");
-    subtitle.textContent = courses[index].name;
-
-    // Update course duration
-    const courseDuration = card.querySelector(".period");
-    courseDuration.innerHTML = `<img src="images/time.svg" /> ${courses[index].duration.hours} Hours`;
-
-    // Update course difficulty level
-    const courseDifficulty = card.querySelector(".difficulty");
-    const difficulty = courses[index].difficulty_level;
-    if (difficulty === "Easy") {
-      courseDifficulty.innerHTML = `<img src="images/easy.png" /> ${difficulty}`;
-    } else if (difficulty === "Intermediate") {
-      courseDifficulty.innerHTML = `<img src="images/medium.png" /> ${difficulty}`;
-    } else {
-      courseDifficulty.innerHTML = `<img src="images/hard.png" /> ${difficulty}`;
-    }
-
-    // Update course short description
-    const subSubTitle = card.querySelector(".short-description");
-    subSubTitle.textContent = courses[index].short_description;
-  });
-};
+"use strict";
 
 // Add fixed header when scrolling
 window.addEventListener("scroll", function () {
@@ -123,6 +36,37 @@ document
       ddown.style.display = "flex";
     }
   });
+
+// Close modals when clicking outside
+window.onclick = function (event) {
+  if (event.target == document.querySelector(".profile-dropdown")) {
+    ddown.classList.remove("animate__animated", "animate__fadeIn");
+    ddown.style.display = "none";
+  }
+};
+
+// Change image as well as bg color when hovering over the categories buttons
+document.querySelectorAll(".category-btn").forEach(function (btn) {
+  let img = btn.querySelector("img:first-of-type");
+  let img2 = btn.querySelector("img:last-of-type");
+  let span = btn.querySelector("span");
+  const imgName = img.id;
+  btn.addEventListener("mouseover", function () {
+    // Swap image source on hover
+    img.src = `images/${imgName}-white.png`;
+    img2.src = `images/arrow-right-white.png`;
+    span.style.color = "white";
+    btn.style.backgroundColor = "#ff723a";
+  });
+
+  btn.addEventListener("mouseout", function () {
+    // Revert image source on mouseout
+    img.src = `images/${imgName}.png`;
+    img2.src = `images/arrow-right.png`;
+    span.style.color = "black";
+    btn.style.backgroundColor = "white";
+  });
+});
 
 // Hovering effects for socials icons
 document.querySelectorAll(".socials-icon-bg").forEach(function (btn) {
